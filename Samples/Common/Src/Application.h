a8b90ce34e6cbb858f8ca543e7c1cb1da85afa3b
wupeng <wupeng_a2484@virtuos.com.cn>
2022-07-29 22:59:48 +0800

    add main

---
+++ Samples/Common/Src/Application.h
@@ -0,0 +1,27 @@
+#ifndef _APPLICATION_H_
+#define _APPLICATION_H_
+namespace Tiga
+{
+    class Application
+    {
+    private:
+        Application(const Application &);
+        Application &operator=(const Application &);
+
+    public:
+        inline Application(/* args */) {}
+
+        inline virtual ~Application() {}
+
+        inline virtual void Initialize() {}
+
+        inline virtual void Update(float inDeltaTime) {}
+
+        inline virtual void Render(float inAspectRatio) {}
+
+        inline virtual void OnGUI() {}
+
+        inline virtual void Shutdown() {}
+    };
+} // namespace Tiga
+#endif
--- Samples/Common/Src/main.cpp
+++ Samples/Common/Src/main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include "../../Common/Src/Application.h"
+
 int main(int, char **)
 {
     std::cout << "Hello, world!\n";
