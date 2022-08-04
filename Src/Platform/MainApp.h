079bc78d9c6aeb42d2e3f12a1e3caab63c283732
wupeng <wupeng_a2484@virtuos.com.cn>
2022-08-04 21:49:17 +0800

    add new main

---
+++ Src/Platform/MainApp.h
@@ -0,0 +1,10 @@
+#ifndef _MAIN_APP_H_
+#define _MAIN_APP_H_
+#include "../Samples/Application.h"
+
+namespace Tiga
+{
+    int32_t Run(Application *app, int argc, const char *const *argv);
+}
+
+#endif
--- Src/Platform/MainWindowsApp.cpp
+++ Src/Platform/MainWindowsApp.cpp
@@ -7,19 +7,108 @@
 #include <bx/thread.h>
 #include <bx/timer.h>
 #include <bx/uint32_t.h>
+#include <iostream>
 
-#include "WindowHandle.h"
-#include "WindowState.h"
+#include <Windows.h>
 
-WindowHandle CreateWindow(int32_t x, int32_t y, uint32_t width, uint32_t height, uint32_t flags = WINDOW_FLAG_NONE, const char *title = "")
+#include "MainApp.h"
+namespace Tiga
 {
-    // bx::MutexScope scope
-}
+    LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 
-void DestroyWindow(WindowHandle handle){}
-void SetWindowPos(WindowHandle handle, int32_t x, int32_t y){}
-void SetWindowSize(WindowHandle handle, uint32_t width, uint32_t height){}
-void SetWindowFlags(WindowHandle handle, uint32_t flags, bool enabled){}
-void SetMouseLock(WindowHandle handle, bool lock){}
-void SetCurrentDir(const char *dir){}
+    int32_t Run(Application *app, int argc, const char *const *argv)
+    {
+        HINSTANCE instance = (HINSTANCE)GetModuleHandle(NULL);
+
+        WNDCLASSEXW wnd;
+        wnd.cbSize = sizeof(wnd);
+        wnd.style = CS_HREDRAW | CS_VREDRAW;
+        wnd.lpfnWndProc = WndProc;
+        wnd.hInstance = instance;
+        wnd.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+        wnd.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+        wnd.hCursor = LoadCursor(NULL, IDC_ARROW);
+        wnd.lpszClassName = L"Tiga";
+        RegisterClassExW(&wnd);
+
+        int screenWidth = GetSystemMetrics(SM_CXSCREEN);
+        int screenHeight = GetSystemMetrics(SM_CYSCREEN);
+        int clientWidth = 800;
+        int clientHeight = 600;
+        RECT windowRect;
+        SetRect(&windowRect, (screenWidth / 2) - (clientWidth / 2),
+                (screenHeight / 2) - (clientHeight / 2),
+                (screenWidth / 2) + (clientWidth / 2),
+                (screenHeight / 2) + (clientHeight / 2));
+
+        // WS_THICKFRAME to resize
+        DWORD style = (WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_MINIMIZEBOX | WS_MAXIMIZEBOX);
+        HWND hwnd = CreateWindowExA(WS_EX_ACCEPTFILES, "Tiga", "Tiga", style,
+                                    windowRect.left, windowRect.top,
+                                    windowRect.right - windowRect.left, windowRect.bottom - windowRect.top,
+                                    NULL, NULL, instance, 0);
+
+        // set platform window data
+        bgfx::PlatformData pd;
+        bx::memSet(&pd, 0, sizeof(pd));
+        pd.nwh = hwnd;
+        bgfx::setPlatformData(pd);
+
+        ShowWindow(hwnd, SW_SHOW);
+        UpdateWindow(hwnd);
+        app->Initialize();
+
+        bgfx::renderFrame();
+
+        MSG msg;
+        DWORD lastTick = GetTickCount();
+        while (true)
+        {
+            bgfx::renderFrame();
+#pragma region Events
+            if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+            {
+                if (msg.message == WM_QUIT)
+                {
+                    break;
+                }
+                TranslateMessage(&msg);
+                DispatchMessage(&msg);
+            }
+#pragma endregion Events
+
+#pragma region Update
+            DWORD thisTick = GetTickCount();
+            float deltaTime = float(thisTick - lastTick) * 0.001f;
+            lastTick = thisTick;
+            app->Update(deltaTime);
+#pragma endregion Update
+
+#pragma region Render
+            app->Render(deltaTime);
+
+            // Set view 0 default viewport.
+            // bgfx::setViewRect(0, 0, 0, clientWidth, clientHeight);
+            // This dummy draw call is here to make sure that view 0 is cleared
+            // if no other draw calls are submitted to view 0.
+            // bgfx::touch(0);
+            // Advance to next frame. Rendering thread will be kicked to
+            // process submitted rendering primitives.
+            bgfx::frame();
+#pragma endregion Render
+
+#pragma region IMGUI Update
+            app->OnGUI();
+#pragma endregion IMGUI Update
+        }
+
+        return (int)msg.wParam;
+    }
+
+    LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
+    {
+        return DefWindowProc(hwnd, iMsg, wParam, lParam);
+    }
+} // namespace Tiga
+
 #endif
