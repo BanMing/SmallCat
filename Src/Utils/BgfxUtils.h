e7ad98afc649981d7e559eacd25ffae73d807b85
wupeng <ban-ming@foxmail.com>
2022-09-04 00:43:53 +0800

    add new struct

--- Src/Utils/BgfxUtils.h
+++
@@ -1,9 +0,0 @@
-#ifndef _BGFXUILS_H_
-#define _BGFXUILS_H_
-#include <bx/allocator.h>
-namespace Tiga
-{
-    bx::AllocatorI *GetDefaultAllocator();
-    bx::AllocatorI *GetAllocator();
-}
-#endif //_BGFXUILS_H_
--- Src/main.cpp
+++ Src/main.cpp
@@ -4,12 +4,6 @@
 #include <bgfx_utils.h>
 #include <imgui/imgui.h>
 
-// int main(int argc, const char **argv)
-// {
-//     Tiga::Application *application = new Tiga::HelloSample();
-//     return Tiga::Run(application, argc, argv);
-// }
-
 class HelloWorld : public entry ::AppI
 {
 protected:
@@ -71,6 +65,48 @@ public:
                                 (m_mouseState.m_buttons[entry::MouseButton::Right] ? IMGUI_MBUT_RIGHT : 0) |
                                 (m_mouseState.m_buttons[entry::MouseButton::Middle] ? IMGUI_MBUT_MIDDLE : 0),
                             m_mouseState.m_mz, uint16_t(m_width), uint16_t(m_height));
+
+            showExampleDialog(this);
+
+            imguiEndFrame();
+
+            // Set view 0 default viewport.
+            bgfx::setViewRect(0, 0, 0, uint16_t(m_width), uint16_t(m_height));
+
+            // This dummy draw call is here to make sure that view 0 is cleard
+            // if no other draw calls are submitted to view 0
+            bgfx::touch(0);
+
+            // Use debug font to print information about this example.
+            bgfx::dbgTextClear();
+            // bgfx::dbgTextImage(
+            //     bx::max<uint16_t>(uint16_t(m_width / 2 / 8), 20) - 20, bx::max<uint16_t>(uint16_t(m_height / 2 / 16), 6) - 6, 40, 12, s_logo, 160);
+            bgfx::dbgTextPrintf(0, 1, 0x0f, "Color can be changed with ANSI \x1b[9;me\x1b[10;ms\x1b[11;mc\x1b[12;ma\x1b[13;mp\x1b[14;me\x1b[0m code too.");
+
+            bgfx::dbgTextPrintf(80, 1, 0x0f, "\x1b[;0m    \x1b[;1m    \x1b[; 2m    \x1b[; 3m    \x1b[; 4m    \x1b[; 5m    \x1b[; 6m    \x1b[; 7m    \x1b[0m");
+            bgfx::dbgTextPrintf(80, 2, 0x0f, "\x1b[;8m    \x1b[;9m    \x1b[;10m    \x1b[;11m    \x1b[;12m    \x1b[;13m    \x1b[;14m    \x1b[;15m    \x1b[0m");
+
+            // Advance to next frame. Rendering thread will be kicked to
+            // process submitted rendering primitives.
+            bgfx::frame();
+
+            return true;
         }
+
+        return false;
     }
 };
+
+int _main_(int _argc, char **_argv)
+{
+    HelloWorld *app = new HelloWorld("Hello World", "Initialization and debug text.", "https://bkaradzic.github.io/bgfx/examples.html#helloworld");
+    app->init(_argc, _argv, 600, 600);
+
+    while (app->update())
+    {
+        }
+    app->shutdown();
+    delete app;
+    return true;
+}
+// ENTRY_IMPLEMENT_MAIN(HelloWorld, "Hello World", "Initialization and debug text.", "https://bkaradzic.github.io/bgfx/examples.html#helloworld");
